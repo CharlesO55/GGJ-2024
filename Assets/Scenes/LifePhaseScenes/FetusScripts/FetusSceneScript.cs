10ca82c54d432d04c15744bb648325c744989093
paolim008 <pao_lim@dlsu.edu.ph>
2024-01-27 12:49:27 +0800

    fetus scene final push
    ART NEEDED

--- Assets/Scenes/LifePhaseScenes/FetusScripts/FetusSceneScript.cs
+++ Assets/Scenes/LifePhaseScenes/FetusScripts/FetusSceneScript.cs
@@ -1,17 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
+using UnityEngine.UI;
 
 public class FetusSceneScript : MonoBehaviour
 {
-    [SerializeField] private float timer = 20f;
+    public static event EventHandler OnSpacePressed;
+    public static event EventHandler OnPhaseEnd;
+    private int progression = 0;
+    private int maxProgression = 5;
 
-    // Update is called once per frame
+    [SerializeField] private TextMeshProUGUI progressionUI;
+    [SerializeField] private Image fetusBackgroundImage;
+
+    private void Start()
+    {
+        FetusSpaceScript.OnInputSuccess += FetusBarScript_OnInputSuccess;
+        FetusSpaceScript.OnInputFailed += FetusBarScript_OnInputFailed;
+    }
+
+    private void FetusBarScript_OnInputFailed(object sender, EventArgs e)
+    {
+        progression -= 2;
+        progression = Mathf.Clamp(progression ,0, 3);
+        progressionUI.text = progression.ToString();
+        // todo - fetusBackgroundImage.sprite = fetusSprite[progression];
+    }
+
+    private void FetusBarScript_OnInputSuccess(object sender, EventArgs e)
+    {
+        progression++;
+        progression = Mathf.Clamp(progression ,0, 3);
+        progressionUI.text = progression.ToString();
+        // todo - fetusBackgroundImage.sprite = fetusSprite[progression];
+        if (CheckProgression())
+        {
+            OnPhaseEnd?.Invoke(this, EventArgs.Empty);
+            //todo - sceneloader next scene
+        }
+    }
+
     void Update()
     {
-        if (timer > 0) timer -= Time.deltaTime;
-        else{ 
-            Debug.Log("ChangeScene");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            OnSpacePressed?.Invoke(this, EventArgs.Empty);
         }
+        
+    }
+
+    private bool CheckProgression()
+    {
+        return progression >= 3;
     }
 }
---
+++ Assets/Scenes/LifePhaseScenes/FetusScripts/FetusSpaceScript.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+
+public class FetusSpaceScript : MonoBehaviour
+{
+    public static event EventHandler OnInputSuccess;
+    public static event EventHandler OnInputFailed;
+
+
+    private bool isActive = true;
+    private float timer;
+    private float spaceTimer;
+    private float spaceTimerMax;
+
+    [SerializeField] private GameObject spaceIndicator;
+    [SerializeField] private Image spaceIndicatorFill;
+    
+    [Serializable]
+    private struct TimerBounds
+    {
+        public float min;
+        public float max;
+    }
+
+    [SerializeField] private TimerBounds timerBounds;
+    [SerializeField] private TimerBounds spaceTimerBounds;
+    
+    private void Awake()
+    {
+        InitializeTimer();
+    }
+
+    private void Start()
+    {
+        FetusSceneScript.OnPhaseEnd += FetusSceneScript_OnPhaseEnd;
+    }
+
+    private void FetusSceneScript_OnPhaseEnd(object sender, EventArgs e)
+    {
+        isActive = false;
+        spaceIndicator.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!isActive) return;
+        
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            if (!spaceIndicator.activeSelf)
+            {
+                spaceIndicator.SetActive(true);
+                InitializeSpaceTimer();
+            }
+
+            if (spaceTimer > 0)
+            {
+                spaceTimer -= Time.deltaTime;
+                spaceIndicatorFill.fillAmount = Mathf.Abs((spaceTimer / spaceTimerMax)-1);
+                if(Input.GetKeyDown(KeyCode.Space))
+                {
+                    spaceIndicator.SetActive(false);
+                    OnInputSuccess?.Invoke(this, EventArgs.Empty);
+                    InitializeTimer();
+                }
+            }
+            else
+            {
+                OnInputFailed?.Invoke(this, EventArgs.Empty);
+                spaceIndicator.SetActive(false);
+                InitializeTimer();
+            }
+        }
+        
+    }
+
+    private void InitializeTimer()
+    {
+        timer = Random.Range(timerBounds.min, timerBounds.max);
+    }
+
+    private void InitializeSpaceTimer()
+    {
+        spaceTimer = Random.Range(spaceTimerBounds.min, spaceTimerBounds.max);
+        spaceTimerMax = spaceTimer;
+    }
+}
