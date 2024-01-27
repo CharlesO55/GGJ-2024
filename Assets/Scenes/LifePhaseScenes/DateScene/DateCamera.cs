fe1d8402f68f89e95762cadb4f3a8797a7ab2008
CharlesO55 <charles_mathew_ong@dlsu.edu.ph>
2024-01-27 17:19:17 +0800

    WIP Date scene

---
+++ Assets/Scenes/LifePhaseScenes/DateScene/DateCamera.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class DateCamera : MonoBehaviour
+{
+    [SerializeField] float m_maxZoom = 1;
+    [SerializeField] float m_minZoom = 5;
+    [SerializeField] private float m_zoomSpeed = 2;
+
+    private CinemachineVirtualCamera m_cam;
+
+    void Start()
+    {
+        this.m_cam = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    void LateUpdate()
+    {
+        ZoomIn();
+
+
+    }
+
+    void ZoomIn()
+    {
+        float currZoom = this.m_cam.m_Lens.OrthographicSize;
+
+        currZoom = Mathf.Lerp(
+            currZoom, 
+            currZoom + m_zoomSpeed * Time.deltaTime * -1, 0.5f);
+
+        currZoom = Mathf.Clamp(currZoom, m_maxZoom, m_minZoom);
+
+        this.m_cam.m_Lens.OrthographicSize = currZoom;
+    }
+}
---
+++ Assets/Scenes/LifePhaseScenes/DateScene/MouseFollower.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    public GameObject m_targetObject = null;
+
+    [SerializeField] float m_speed = 1f;
+
+    [SerializeField] private float m_edgeSize = 20f;
+
+
+    [SerializeField] private Vector2 m_transformLimit = new Vector2(7.75f, 1.2f);
+
+
+    void Update()
+    {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        if (hit != null && hit.collider != null)
+        {
+            Debug.Log("Hit" + hit.collider.name);
+        }
+    }
+    void LateUpdate()
+    {
+        ScrollMovement();
+    }
+
+    void ScrollMovement()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 moveBy = this.transform.position;
+
+
+        //Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Camera.main.nearClipPlane));
+
+        //Debug.Log( Camera.main.ViewportToWorldPoint(new Vector3(-1, -1, Camera.main.nearClipPlane)));
+
+        if (mousePos.x < m_edgeSize && moveBy.x > -m_transformLimit.x)
+        {
+            moveBy.x -= Time.deltaTime * m_speed;
+        }
+        else if (mousePos.x > Screen.width - m_edgeSize && moveBy.x < m_transformLimit.x)
+        {
+            moveBy.x += Time.deltaTime * m_speed;
+        }
+
+        if (mousePos.y < m_edgeSize && moveBy.y > -m_transformLimit.y)
+        {
+            moveBy.y -= Time.deltaTime * m_speed;
+        }
+        else if (mousePos.y > Screen.height - m_edgeSize && moveBy.y < m_transformLimit.y)
+        {
+            moveBy.y += Time.deltaTime * m_speed;
+        }
+
+        this.transform.position = moveBy;
+    }
+}
