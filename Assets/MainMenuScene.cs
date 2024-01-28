01b504ac6295f94db8eb1a9fd94ab347a8b4a89b
paolim008 <pao_lim@dlsu.edu.ph>
2024-01-28 12:29:31 +0800

    mainmenuscene

---
+++ Assets/MainMenuScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+            else SceneManager.LoadScene(1);
+        }
+    }
+}
